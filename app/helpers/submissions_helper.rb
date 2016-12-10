c992b859f67a8458f66bd6c6effefc570b9248f1
BigMcLargeHuge <hagen.sam@gmail.com>
2016-12-10 04:30:04 -0700

    added select boxes

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,5 @@
 module SubmissionsHelper
+	def source_type
+		["Fake News (fabricated information, or grossly distort actual news)", "Satire (meant to be humorous)", "Extreme Bias (uses information to editorialize rather than inform)", "Conspiracy Theory", "Rumor Mill (traffics unvalidated gossip)", "Junk Science (discusses unvalidated science)", "Hate Group (promotes discrimination such as racism)", "Clickbait (uses exaggerated, misleading, or questionable headlines)", "Unreliable (content is often false or mistaken)"]
+	end
 end
