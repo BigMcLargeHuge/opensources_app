2a1ad5bc62e6be261264f7914288ab83ec7040cf
BigMcLargeHuge <hagen.sam@gmail.com>
2016-12-20 02:24:25 -0700

    removed substrings from domain submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,4 +4,13 @@ class Submission < ActiveRecord::Base
 	has_and_belongs_to_many :users
 
 	attr_accessor :fresh
+
+	before_save do
+		the_domain = self.domain
+		the_domain = the_domain.remove("https://")
+		the_domain = the_domain.remove("http://")
+		the_domain = the_domain.remove("www.")
+		the_domain = the_domain.remove(" ")
+		self.domain = the_domain
+	end
 end
