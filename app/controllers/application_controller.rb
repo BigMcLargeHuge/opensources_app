94dbcd6c39e285943eb540ff03ae6b21ab495c16
BigMcLargeHuge <hagen.sam@gmail.com>
2017-01-08 19:49:22 -0700

    added alert styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to main_app.root_url, :alert => "Please sign in first." }
+    end
+  end
+
   protected
   def authenticate_user!
     if user_signed_in?
