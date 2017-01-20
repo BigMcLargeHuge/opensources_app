07f83e4fc1a2800ed0b58ab8eaf3c2a4b128471d
BigMcLargeHuge <hagen.sam@gmail.com>
2017-01-20 05:55:06 -0700

    added tracker code for google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
+  GA.tracker = ENV["TRACKER"]
+
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
